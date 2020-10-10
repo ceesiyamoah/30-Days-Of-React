6dbf4961f81379f5c8ca9bfd41d83d1edcae811b
ceesiyamoah <ccyamoah@gmail.com>
2020-10-10 23:21:34 +0000

    hexadecimal color generator

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,86 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import profileThumbnail from './images/img.jpg';
 
-const skills = [
-	'HTML',
-	'CSS',
-	'JS',
-	'React',
-	'Azure',
-	'Python',
-	'Data Analysis',
-	'MySQL',
-	'Numpy',
-	'Pandas',
-	'Git',
-	'C++',
-	'C',
-	'OS',
-	'HTML',
-	'CSS',
-	'JS',
-	'React',
-	'Azure',
-	'Python',
-	'Data Analysis',
-	'MySQL',
-	'Numpy',
-	'Pandas',
-	'Git',
-	'C++',
-	'C',
-	'OS',
-];
-const Thumbnail = () => (
-	<img
-		src={profileThumbnail}
-		alt=' User Thumbnail'
-		className='user-thumbnail'
-	/>
-);
-const Name = () => (
-	<div className='name-wrapper'>
-		<span className='name'>CYRIL YAMOAH</span>
-		<img
-			src='https://img.icons8.com/color/48/000000/checked--v1.png'
-			className='verified'
-			alt='verified tick'
-		/>
-	</div>
-);
-const Job = () => <span className='job'>Junior Developer, Ghana</span>;
-const GetSkills = () =>
-	skills.map((skill) => (
-		<span key={skill} className='skill'>
-			{skill}
-		</span>
-	));
-const Skills = () => (
-	<div className='skills-wrapper'>
-		<span className='name'>Skills</span>
-		<div className='skill-wrapper'>
-			<GetSkills />
-		</div>
-	</div>
-);
-const Joined = () => (
-	<div className='end'>
-		<img
-			src='https://img.icons8.com/pastel-glyph/64/000000/clock.png'
-			alt='Clock'
-		/>
-		<span>Joined on September 3, 2020</span>
+const getHexa = () => {
+	const chars = 'abcdef0123456789';
+	let hexa = [];
+	for (let index = 0; index < 6; index++) {
+		hexa += chars[parseInt(Math.random() * chars.length)];
+	}
+	return '#'.concat(hexa);
+};
+
+const HexaColor = () => (
+	<div className='color-holder'>
+		<div style={{ backgroundColor: getHexa() }}>{getHexa()}</div>
 	</div>
 );
 
 const App = () => (
-	<div className='card'>
-		<Thumbnail />
-		<Name />
-		<Job />
-		<Skills />
-		<Joined />
+	<div className='app'>
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
 	</div>
 );
 
