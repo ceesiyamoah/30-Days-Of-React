f5df77a4f03c1b7d0b4f8a239fa33e6012b93416
ceesiyamoah <ccyamoah@gmail.com>
2020-10-10 17:32:58 +0000

    subscribe card

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,73 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import profileThumbnail from './images/img.jpg';
-
-const skills = [
-	'HTML',
-	'CSS',
-	'JS',
-	'React',
-	'Azure',
-	'Python',
-	'Data Analysis',
-	'MySQL',
-	'Numpy',
-	'Pandas',
-	'Git',
-	'C++',
-	'C',
-	'OS',
-	'HTML',
-	'CSS',
-	'JS',
-	'React',
-	'Azure',
-	'Python',
-	'Data Analysis',
-	'MySQL',
-	'Numpy',
-	'Pandas',
-	'Git',
-	'C++',
-	'C',
-	'OS',
-];
 
 const app = (
-	<div className='card'>
-		<img
-			src={profileThumbnail}
-			alt=' User Thumbnail'
-			className='user-thumbnail'
-		/>
-		{/* TODO improve how the text and the icon work */}
-		<div className='name-wrapper'>
-			<span className='name'>CYRIL YAMOAH</span>
-			<img
-				src='https://img.icons8.com/color/48/000000/checked--v1.png'
-				className='verified'
-				alt='verified tick'
-			/>
+	<div className='subscribe'>
+		<h1 className='subscribe-title'>SUBSCRIBE</h1>
+		<span className='subscribe-text'>
+			Sign up with your email address to receive news and updates
+		</span>
+		<div className='subscribe-form'>
+			<input placeholder='First name' className='subscribe-form-input' />
+			<input placeholder='Last name' className='subscribe-form-input' />
+			<input placeholder='Email' className='subscribe-form-input' />
 		</div>
-		<span className='job'>Junior Developer, Ghana</span>
-		<div className='skills-wrapper'>
-			<span className='name'>Skills</span>
-			<div className='skill-wrapper'>
-				{skills.map((skill) => (
-					<span key={skill} className='skill'>
-						{skill}
-					</span>
-				))}
-			</div>
-		</div>
-		<div className='end'>
-			{/* TODO improve how the text and the icon work */}
-			<img
-				src='https://img.icons8.com/pastel-glyph/64/000000/clock.png'
-				alt='Clock'
-			/>
-			<span>Joined on September 3, 2020</span>
-		</div>
+		<button className='subscribe-btn'>Subscribe</button>
 	</div>
 );
 
