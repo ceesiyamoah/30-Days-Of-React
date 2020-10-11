764b8a7021cd4995db0f167da42c260199747d4b
ceesiyamoah <ccyamoah@gmail.com>
2020-10-11 00:23:12 +0000

    merger

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const app = (
-	<div className='subscribe'>
-		<h1 className='subscribe-title'>SUBSCRIBE</h1>
-		<span className='subscribe-text'>
-			Sign up with your email address to receive news and updates
-		</span>
-		<div className='subscribe-form'>
-			<input placeholder='First name' className='subscribe-form-input' />
-			<input placeholder='Last name' className='subscribe-form-input' />
-			<input placeholder='Email' className='subscribe-form-input' />
+const getHexa = () => {
+	const chars = 'abcdef0123456789';
+	let hexa = [];
+	for (let index = 0; index < 6; index++) {
+		hexa += chars[parseInt(Math.random() * chars.length)];
+	}
+	return '#'.concat(hexa);
+};
+
+const HexaColor = () => {
+	const color = getHexa();
+	console.log(color);
+	return (
+		<div className='color-holder'>
+			<div style={{ backgroundColor: color }}>{color}</div>
 		</div>
-		<button className='subscribe-btn'>Subscribe</button>
+	);
+};
+
+const App = () => (
+	<div className='app'>
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
+		<HexaColor />
 	</div>
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
