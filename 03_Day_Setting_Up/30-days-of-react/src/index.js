1b90009a93d75c4c017f51224c2d45df34daffab
ceesiyamoah <ccyamoah@gmail.com>
2020-10-11 21:55:15 +0000

    countries with population stats

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,58 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { countriesData } from './countries';
 
 const data = {
 	header: {
-		title: '30 Days Of React',
-		subtitle: 'Hexadecimal Colors',
+		headerTitle: '30 Days Of React',
+		subtitle: 'World Population',
 	},
-	numberOfCells: 32,
+	graph: {
+		graphTitle: 'Ten most populated countries',
+		countries: countriesData,
+	},
 };
+const commarize = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-const getHex1 = () => {
-	const chars = '0123456789abcdef';
-	const hex = [];
-	for (let i = 0; i < 6; i++) {
-		hex.push(chars[parseInt(Math.random() * 16)]);
-	}
-	return '#' + hex.join('');
-};
-const Header = ({ header: { title, subtitle } }) => {
+const Header = ({ title, subtitle, size = '70px' }) => {
 	return (
 		<div className='header-wrapper'>
-			<span className='header-title'>{title}</span>
+			<span className='header-title' style={{ fontSize: size }}>
+				{title}
+			</span>
 			<span className='header-subtitle'>{subtitle}</span>
 		</div>
 	);
 };
-const Grid = ({ getHex, numberOfCells }) => {
-	const cellsArray = [];
-	for (let i = 0; i < numberOfCells; i++) {
-		cellsArray.push(i);
-	}
-	return (
-		<div className='grid-wrapper'>
-			{cellsArray.map((cell) => (
-				<Cell getHex={getHex} key={cell} />
-			))}
-		</div>
-	);
-};
-const Cell = ({ getHex }) => {
-	const color = getHex();
-	return (
-		<div className='cell-wrapper' style={{ backgroundColor: color }}>
-			{color}
-		</div>
-	);
-};
-const App = ({ data: { header, numberOfCells }, getHex }) => (
+const Population = ({ countries, total }) => (
 	<div>
-		<Header header={header} />
-		<Grid getHex={getHex} numberOfCells={numberOfCells} />
+		<div className='popu-wrapper'>
+			<span className='popu-countryname'>World</span>
+			<div className='popu-length'>
+				<div className='popu-length-bar'>&nbsp; </div>
+			</div>
+			<div className='popu-value'>{commarize(total)}</div>
+		</div>
+		{countries.map((country) => (
+			<div className='popu-wrapper'>
+				<span className='popu-countryname'>{country.name}</span>
+				<div className='popu-length'>
+					<div
+						className='popu-length-bar'
+						style={{ width: `${(country.population / total) * 100}%` }}
+					>
+						&nbsp;{' '}
+					</div>
+				</div>
+				<div className='popu-value'>{commarize(country.population)}</div>
+			</div>
+		))}
 	</div>
 );
-ReactDOM.render(
-	<App data={data} getHex={getHex1} />,
-	document.getElementById('root')
+
+const Graph = ({ graph: { graphTitle, countries } }) => {
+	countries.sort(
+		(country1, country2) => country2.population - country1.population
+	);
+	const popTotal = countries.reduce(
+		(total, item) => (total += item.population),
+		0
+	);
+
+	const countriesList = countries.slice(0, 9);
+	return (
+		<div>
+			<Header title={graphTitle} size='10px' />
+			<Population countries={countriesList} total={popTotal} />
+		</div>
+	);
+};
+
+const App = ({
+	data: {
+		header: { headerTitle, subtitle },
+		graph,
+	},
+}) => (
+	<div>
+		<Header title={headerTitle} subtitle={subtitle} />
+		<Graph graph={graph} />
+	</div>
 );
+
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
