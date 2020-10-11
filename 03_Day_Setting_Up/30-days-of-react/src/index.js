51782ebf296dedb1544b9ea8d13ad74e588fcdfc
ceesiyamoah <ccyamoah@gmail.com>
2020-10-11 01:10:17 +0000

    user card props solution

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,23 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import cssLogo from './images/css_logo.png';
-import htmlLogo from './images/html_logo.png';
-import reactLogo from './images/react_logo.png';
+import profileThumbnail from './images/img.jpg';
 
-const title = 'Front End Technologies';
-const data = {
-	title: title,
-	images: [htmlLogo, cssLogo, reactLogo],
-};
-const App = ({ data: { title, images } }) => (
-	<div>
-		<p className='title'>{title}</p>
-		<div className='main'>
-			{images.map((image) => (
-				<img src={image} key={image} alt='technology' />
-			))}
+const skills = [
+	'HTML',
+	'CSS',
+	'JS',
+	'React',
+	'Azure',
+	'Python',
+	'Data Analysis',
+	'MySQL',
+	'Numpy',
+	'Pandas',
+	'Git',
+	'C++',
+	'C',
+	'OS',
+	'HTML1',
+	'CSS1',
+	'JS1',
+	'React1',
+	'Azure1',
+	'Python1',
+	'Data Analysis1',
+	'MySQL1',
+	'Numpy1',
+	'Pandas1',
+	'Git1',
+	'C++1',
+	'C1',
+	'OS1',
+];
+
+const dateToday = new Date();
+
+const Thumbnail = ({ imgSrc }) => (
+	<img src={imgSrc} alt=' User Thumbnail' className='user-thumbnail' />
+);
+const Name = ({ name }) => (
+	<div className='name-wrapper'>
+		<span className='name'>{name}</span>
+		<img
+			src='https://img.icons8.com/color/48/000000/checked--v1.png'
+			className='verified'
+			alt='verified tick'
+		/>
+	</div>
+);
+const Job = ({ title, location }) => (
+	<span className='job'>{title + ', ' + location}</span>
+);
+const GetSkills = ({ skills }) =>
+	skills.map((skill) => (
+		<span key={skill} className='skill'>
+			{skill}
+		</span>
+	));
+const Skills = ({ skills }) => (
+	<div className='skills-wrapper'>
+		<span className='name'>Skills</span>
+		<div className='skill-wrapper'>
+			<GetSkills skills={skills} />
 		</div>
 	</div>
 );
+const Joined = ({ date }) => (
+	<div className='end'>
+		<img
+			src='https://img.icons8.com/pastel-glyph/64/000000/clock.png'
+			alt='Clock'
+		/>
+		<span>Joined on {date}</span>
+	</div>
+);
 
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
+const App = () => (
+	<div className='card'>
+		<Thumbnail imgSrc={profileThumbnail} />
+		<Name name='CYRIL YAMOAH' />
+		<Job title='Junior Developer' location='Ghana' />
+		<Skills skills={skills} />
+		<Joined date={dateToday.toLocaleDateString()} />
+	</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
