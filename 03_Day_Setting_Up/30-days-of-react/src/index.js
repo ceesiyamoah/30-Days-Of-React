6f02bf7dae060a0848092ea9f3a4692a2194486f
ceesiyamoah <ccyamoah@gmail.com>
2020-10-13 00:59:35 +0000

    Toggle theme implemented

--- 03_Day_Setting_Up/30-days-of-react/src/index.js
+++ 03_Day_Setting_Up/30-days-of-react/src/index.js
@@ -1,83 +1,141 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { countriesData } from './countries';
+import cyrilImage from './images/img.jpg';
 
-const data = {
+//* * Improve implementation of theme toggle
+
+const Header = ({
 	header: {
-		headerTitle: '30 Days Of React',
-		subtitle: 'World Population',
+		headerTitle,
+		headerSubtile,
+		authorName,
+		date,
+		theme: { backgroundColor, fontColor },
 	},
-	graph: {
-		graphTitle: 'Ten most populated countries',
-		countries: countriesData,
-	},
-};
-const commarize = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-const Header = ({ title, subtitle, size = '70px' }) => {
+}) => {
 	return (
-		<div className='header-wrapper'>
-			<span className='header-title' style={{ fontSize: size }}>
-				{title}
-			</span>
-			<span className='header-subtitle'>{subtitle}</span>
-		</div>
-	);
-};
-const Population = ({ countries, total }) => (
-	<div>
-		<div className='popu-wrapper'>
-			<span className='popu-countryname'>World</span>
-			<div className='popu-length'>
-				<div className='popu-length-bar'>&nbsp; </div>
-			</div>
-			<div className='popu-value'>{commarize(total)}</div>
-		</div>
-		{countries.map((country) => (
-			<div className='popu-wrapper'>
-				<span className='popu-countryname'>{country.name}</span>
-				<div className='popu-length'>
-					<div
-						className='popu-length-bar'
-						style={{ width: `${(country.population / total) * 100}%` }}
-					>
-						&nbsp;{' '}
-					</div>
-				</div>
-				<div className='popu-value'>{commarize(country.population)}</div>
-			</div>
-		))}
-	</div>
-);
-
-const Graph = ({ graph: { graphTitle, countries } }) => {
-	countries.sort(
-		(country1, country2) => country2.population - country1.population
-	);
-	const popTotal = countries.reduce(
-		(total, item) => (total += item.population),
-		0
-	);
-
-	const countriesList = countries.slice(0, 9);
-	return (
-		<div>
-			<Header title={graphTitle} size='10px' />
-			<Population countries={countriesList} total={popTotal} />
+		<div
+			className='header-wrapper'
+			style={{ backgroundColor: backgroundColor, color: fontColor }}
+		>
+			<span className='header-title'>{headerTitle}</span>
+			<span className='header-subtitle'>{headerSubtile}</span>
+			<span>{authorName}</span>
+			<span>{date}</span>
 		</div>
 	);
 };
 
-const App = ({
-	data: {
-		header: { headerTitle, subtitle },
-		graph,
+const Pre = ({
+	requirements: {
+		requirements,
+		theme: { backgroundColor },
 	},
-}) => (
-	<div>
-		<Header title={headerTitle} subtitle={subtitle} />
-		<Graph graph={graph} />
-	</div>
-);
+}) => {
+	let bgColor =
+		'rgb(7, 175, 218)' === backgroundColor ? 'white' : 'rgb(10, 3, 112)';
+	return (
+		<div className='req-wrapper' style={{ backgroundColor: bgColor }}>
+			<span>Prerequisites for react.js are</span>
+			<ul className='req-list'>
+				{requirements.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
+const Profile = ({ details: { image, authorName }, bgColor }) => {
+	let bacgColor = 'rgb(7, 175, 218)' === bgColor ? 'white' : 'rgb(10, 3, 112)';
+	return (
+		<div className='profile-wrapper' style={{ backgroundColor: bacgColor }}>
+			<img src={image} alt='profile' className='profile-image' />
+			<span>{authorName}</span>
+		</div>
+	);
+};
+
+const Footer = ({ year, bgColor }) => {
+	return (
+		<div className='footer' style={{ backgroundColor: bgColor }}>
+			&copy; {year}
+		</div>
+	);
+};
+
+const ButtonGroup = ({ changeTheme, bgColor }) => {
+	let bacgColor = 'rgb(7, 175, 218)' === bgColor ? 'white' : 'rgb(10, 3, 112)';
+	return (
+		<div className='btn-wrapper' style={{ backgroundColor: bacgColor }}>
+			<button
+				onClick={() => {
+					alert('Good day');
+				}}
+			>
+				Greet People
+			</button>{' '}
+			<button
+				onClick={() => {
+					alert(new Date().toTimeString());
+				}}
+			>
+				Show Time
+			</button>{' '}
+			<button onClick={changeTheme}>Change Theme</button>
+		</div>
+	);
+};
+
+class App extends Component {
+	state = {
+		bgColor: 'rgb(7, 175, 218)',
+		fontColor: 'black',
+	};
+	changeTheme = () => {
+		let newColor = [];
+		if (this.state.bgColor === 'rgb(7, 175, 218)') {
+			newColor = ['rgb(10, 3, 112)', 'white'];
+		} else {
+			newColor = ['rgb(7, 175, 218)', 'black'];
+		}
+		this.setState({ bgColor: newColor[0], fontColor: newColor[1] });
+		console.log(newColor);
+	};
+	render() {
+		const data = {
+			headerTitle: 'Welcome to 30 days of React',
+			headerSubtile: 'Getting Started with React',
+			authorName: 'Cyril Yamoah',
+			date: new Date().toDateString(),
+			requirements: ['HTML', 'CSS', 'JavaScript'],
+			image: cyrilImage,
+			theme: {
+				backgroundColor: this.state.bgColor,
+				fontColor: this.state.fontColor,
+			},
+		};
+
+		return (
+			<div
+				className='App'
+				style={{
+					backgroundColor: this.state.bgColor,
+					color: this.state.fontColor,
+				}}
+			>
+				<Header header={data} />
+				<Pre requirements={data} />
+				<Profile details={data} bgColor={this.state.bgColor} />
+				<ButtonGroup
+					changeTheme={this.changeTheme}
+					bgColor={this.state.bgColor}
+				/>
+
+				<Footer year='2020' bgColor={this.state.bgColor} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
