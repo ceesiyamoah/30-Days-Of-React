6d3373efe32ca22ede43b011a61fa0d8941c36b1
ceesiyamoah <ccyamoah@gmail.com>
2020-10-19 00:55:37 +0000

    added to gitignore file

--- 03_Day_Setting_Up/30-days-of-react/src/App.js
+++ 03_Day_Setting_Up/30-days-of-react/src/App.js
@@ -5,27 +5,33 @@ export default class App extends Component {
 		super(props);
 		console.log('I am  the constructor and  I will be the first to run.');
 		this.state = {
-			firstName: '',
+			firstName: 'John',
+			day: 1,
 		};
 	}
 
-	static getDerivedStateFromProps(props, state) {
-		console.log(
-			props,
-			state,
-			'I am getDerivedStateFromProps and I will be the second to run.'
-		);
-		return null;
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log(nextProps, nextState);
+		console.log(nextState.day);
+		if (nextState.day > 31) {
+			return false;
+		} else {
+			return true;
+		}
 	}
-	componentDidMount() {
-		console.log('I am componentDidMount and I will be last to run.');
-	}
-
+	// the doChallenge increment the day by one
+	doChallenge = () => {
+		this.setState({
+			day: this.state.day + 1,
+		});
+	};
 	render() {
-		console.log('I am render and I will be the third to run.');
 		return (
 			<div className='App'>
 				<h1>React Component Life Cycle</h1>
+				<button onClick={this.doChallenge}>Do Challenge</button>
+				<p>Challenge: Day {this.state.day}</p>
+				{this.state.congratulate && <h2>{this.state.congratulate}</h2>}
 			</div>
 		);
 	}
